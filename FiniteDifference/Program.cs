76003657b485dc9ede5cf51881ca31654c359178
tk-yoshimura <yoshimura8192@gmail.com>
2021-07-09 17:07:41 +0900

    + results

---
+++ FiniteDifference/Program.cs
@@ -0,0 +1,50 @@
+﻿using MultiPrecision;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FiniteDifference {
+    class Program {
+        static readonly string results_dir = "../../../../results/";
+
+        static void Main(string[] args) {
+            MultiPrecision<Pow2.N8> h = MultiPrecision<Pow2.N8>.Ldexp(1, -36);
+
+            List<(decimal x, MultiPrecision<Pow2.N8>[] ps)> items = ReadTxt();
+
+            using (StreamWriter sw = new(results_dir + "diff_pdf_wolfram_neighbor_finite_diff.txt")) {
+                foreach ((decimal x, MultiPrecision<Pow2.N8>[] ps) in items) {
+                    if (ps[0] >= ps[1] || ps[1] >= ps[2] || ps[2] >= ps[3] || ps[3] >= ps[4]) {
+                        throw new FormatException();
+                    }
+
+                    MultiPrecision<Pow2.N8> g = ((ps[3] - ps[1]) * 2 / 3 - (ps[4] - ps[0]) / 12) / h;
+
+                    sw.WriteLine($"{x},{g:e19}");
+                }
+            }
+        }
+
+        static List<(decimal x, MultiPrecision<Pow2.N8>[] ps)> ReadTxt() {
+            List<(decimal x, MultiPrecision<Pow2.N8>[])> table = new();
+
+            using (StreamReader sr = new(results_dir + "diff_pdf_wolfram_neighbor.txt")) {
+                // skip header 
+                sr.ReadLine();
+                sr.ReadLine();
+
+                while (!sr.EndOfStream) {
+                    string[] xstr = sr.ReadLine().Split('/');
+                    decimal x = xstr.Length == 1 ? decimal.Parse(xstr[0]) : (decimal.Parse(xstr[0]) / decimal.Parse(xstr[1]));
+
+                    MultiPrecision<Pow2.N8>[] ps = sr.ReadLine()[1..^1].Split(',').Select(str => (MultiPrecision<Pow2.N8>)str.Trim()).ToArray();
+
+                    table.Add((x, ps));
+                }
+            }
+
+            return table;
+        }
+    }
+}
--- LandauDistribution/Program.cs
+++ LandauDistribution/Program.cs
@@ -8,24 +8,51 @@ namespace LandauDistribution {
         static readonly string results_dir = "../../../../results/";
 
         static void Main(string[] args) {
-            List<MultiPrecision<Pow2.N8>> xs = new();
+            List<MultiPrecision<Pow2.N4>> xs = new() {
+                //"-2.62916563729442102561",
+                //"-2.44950010103172525499",
+                //"-2.29064564961382595297",
+                //"-2.10489790934939769338",
+                //"-1.79957402870039992314",
+                //"-1.49825415177780273396",
+                //"-1.09225452805484635423",
+                //"-0.77188422256801273027",
+                //"-0.48277711307393456125",
+                //"-0.20464065154575316905",
+                //"0.073877362330763778128",
+                //"0.361135283101730744896",
+                //"0.664768389237266480219",
+                "0.992995803490664167419",
+                "1.35578042099080132503",
+                "1.76629275098246523993",
+                "2.24319477339787288381",
+                "2.81468113996303570812",
+                "3.52636966712139778598",
+                "4.45839461019464834851",
+                "5.76761029823977006164",
+                "7.81274710480049833585",
+                "11.649284684474405569996",
+                "22.450278078872781782888",
+                "43.202525183847707366482",
+                "104.156361812207433543595",
+                "204.862416150123003839536",
+                "405.562095010331006820385",
+                "1006.482330369225631256342",
+            };
 
-            for (decimal x = -3.5m; x >= -4m; x -= 1 / 8m) {
-                xs.Add(x);
-            }
+            using (StreamWriter sw = new StreamWriter(results_dir + "diff_pdf_n4_neg24_pos21_quantiles.csv", append: true)) {
+                //sw.WriteLine("x,pdf'(x),error,accurate_bits");
 
-            for (decimal x = -4.25m; x >= -8m; x -= 1 / 4m) {
-                xs.Add(x);
-            }
-
-            using (StreamWriter sw = new StreamWriter(results_dir + "diff_pdf_n8_neg26_pos20.csv")) {
-                sw.WriteLine("x,pdf'(x),error,accurate_bits");
-
-                foreach (MultiPrecision<Pow2.N8> x in xs) {
-                    MultiPrecision<Pow2.N8> y, error;
+                foreach (MultiPrecision<Pow2.N4> x in xs) {
+                    MultiPrecision<Pow2.N4> y, error;
                     long accurate_bits;
 
-                    (y, error, accurate_bits) = DiffPDFNegativeSide<Pow2.N8>.Value(x, intergrate_iterations: 26);
+                    if (x < 0) {
+                        (y, error, accurate_bits) = DiffPDFNegativeSide<Pow2.N4>.Value(x, intergrate_iterations: 24);
+                    }
+                    else { 
+                        (y, error, accurate_bits) = DiffPDFPositiveSide<Pow2.N4>.Value(x, intergrate_iterations: 21);
+                    }
 
                     Console.WriteLine(y);
                     Console.WriteLine(error);
