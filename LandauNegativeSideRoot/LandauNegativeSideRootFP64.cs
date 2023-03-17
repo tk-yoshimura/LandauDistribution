aff0e2e0a78f65f6ed40cb2953b04a3d4a9b81f2
tk-yoshimura <yoshimura8192@gmail.com>
2023-03-17 14:49:12 +0900

    fix root search

--- LandauNegativeSideRoot/LandauNegativeSideRootFP64.cs
+++ LandauNegativeSideRoot/LandauNegativeSideRootFP64.cs
@@ -1,5 +1,5 @@
 ﻿namespace LandauNegativeSideRoot {
-    public static class LandauNegativeSideInverse {
+    public static class LandauNegativeSideRootFP64 {
         static readonly double c = 2 / Math.PI, logc = Math.Log(c);
 
         /// <summary>solve for t : v = 2t/pi (x + log(t) + log(2/pi))</summary>
@@ -13,7 +13,10 @@
                 dt = dv / df;
 
                 if (double.IsFinite(dt_prev)) {
-                    if (Math.Abs(dt) > Math.Abs(dt_prev) || dt * dt_prev < 0) {
+                    if (Math.Abs(dt) > Math.Abs(dt_prev)) {
+                        dt = Math.CopySign(dt_prev, dt);
+                    }
+                    if(dt * dt_prev < 0) {
                         dt /= 4;
                     }
                 }
@@ -45,7 +48,10 @@
                 dt = dv / df;
 
                 if (double.IsFinite(dt_prev)) {
-                    if (Math.Abs(dt) > Math.Abs(dt_prev) || dt * dt_prev < 0) {
+                    if (Math.Abs(dt) > Math.Abs(dt_prev)) {
+                        dt = Math.CopySign(dt_prev, dt);
+                    }
+                    if(dt * dt_prev < 0) {
                         dt /= 4;
                     }
                 }
@@ -83,24 +89,24 @@
         }
 
         /// <summary>Enumerate cos curve waypoints</summary>
-        public static IEnumerable<double> EnumHalfPIValues(double x, int maxpoints = 32) {
+        public static IEnumerable<(double t, int n)> EnumHalfPIValues(double x, int maxpoints = 32) {
             double t_peak = Peak(x), t_peakval = Func(x, t_peak).v;
             int decrease_points = (int)Math.Floor(-t_peakval * c);
 
             double t = 0d, dt = 1e-2d;
-            yield return t;
+            yield return (t, 0);
             int i, j;
 
-            for (i = 1, j = -1; i <= decrease_points; i++, j--) {
+            for (i = 1, j = -1; i <= decrease_points && i < maxpoints; i++, j--) {
                 double t_next = SearchRootDecreasePhase(x, j * Math.PI / 2, t + dt, t_peak);
                 (t, dt) = (t_next, (t_next - t) / 2);
-                yield return t_next;
+                yield return (t_next, j);
             }
 
             for (j += 1; i < maxpoints; i++, j++) {
                 double t_next = SearchRootIncreasePhase(x, j * Math.PI / 2, t_peak + dt, t_peak);
                 (t, dt) = (t_next, (t_next - t) / 2);
-                yield return t_next;
+                yield return (t_next, j);
             }
 
             yield break;
--- LandauNegativeSideRoot/Program.cs
+++ LandauNegativeSideRoot/Program.cs
@@ -1,15 +1,41 @@
-﻿namespace LandauNegativeSideRoot {
+﻿using MultiPrecision;
+
+namespace LandauNegativeSideRoot {
     class Program {
         static void Main() {
             for (decimal x = -2.1447298858494000m; x >= -2.1447298858494010m; x -= 1e-16m) {
-                List<double> ts = LandauNegativeSideInverse.EnumHalfPIValues((double)x).ToList();
+                List<(MultiPrecision<Pow2.N4> t, int n)> ts = LandauNegativeSideRootMP<Pow2.N4>.EnumHalfPIValues((MultiPrecision<Pow2.N4>)x).ToList();
 
                 Console.WriteLine(x);
 
-                foreach (double t in ts) {
-                    double v = LandauNegativeSideInverse.Func((double)x, t).v;
+                foreach ((MultiPrecision<Pow2.N4> t, int n) in ts) {
+                    MultiPrecision<Pow2.N4> v = LandauNegativeSideRootMP<Pow2.N4>.Func((MultiPrecision<Pow2.N4>)x, t).v;
 
-                    Console.WriteLine($"{t}\t{(v / Math.PI):e15}");
+                    Console.WriteLine($"{t}\t{(v / MultiPrecision<Pow2.N4>.PI)}");
+                }
+            }
+
+            for (decimal x = -2.144m; x >= -2.145m; x -= 1e-5m) {
+                List<(MultiPrecision<Pow2.N4> t, int n)> ts = LandauNegativeSideRootMP<Pow2.N4>.EnumHalfPIValues((MultiPrecision<Pow2.N4>)x).ToList();
+
+                Console.WriteLine(x);
+
+                foreach ((MultiPrecision<Pow2.N4> t, int n) in ts) {
+                    MultiPrecision<Pow2.N4> v = LandauNegativeSideRootMP<Pow2.N4>.Func((MultiPrecision<Pow2.N4>)x, t).v;
+
+                    Console.WriteLine($"{t}\t{(v / MultiPrecision<Pow2.N4>.PI)}");
+                }
+            }
+
+            for (decimal x = -2.0m; x >= -12.0m; x -= 0.01m) {
+                List<(MultiPrecision<Pow2.N4> t, int n)> ts = LandauNegativeSideRootMP<Pow2.N4>.EnumHalfPIValues((MultiPrecision<Pow2.N4>)x).ToList();
+
+                Console.WriteLine(x);
+
+                foreach ((MultiPrecision<Pow2.N4> t, int n) in ts) {
+                    MultiPrecision<Pow2.N4> v = LandauNegativeSideRootMP<Pow2.N4>.Func((MultiPrecision<Pow2.N4>)x, t).v;
+
+                    Console.WriteLine($"{t}\t{(v / MultiPrecision<Pow2.N4>.PI)}");
                 }
             }
 
