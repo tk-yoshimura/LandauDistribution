b57dcb9bfc3880ad560878f513c568626ad4eb0f
tk-yoshimura <yoshimura8192@gmail.com>
2023-05-04 16:35:00 +0900

    fix asminfo

---
+++ Digits100/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LandauDistribution Pade Approximate")]
+[assembly: AssemblyDescription("Landau Distribution Pade Approximate")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LandauDistributionPadeApproximate")]
+[assembly: AssemblyCopyright("Copyright © T.Yoshimura 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.*")]
