3ce922dd7d885ffd00f31d15c801bd8f4c854ae3
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-26 22:08:23 +0900

    fix: comment out

--- LandauEvalPadeApprox/ExpectedQuantileLogLogN16.cs
+++ LandauEvalPadeApprox/ExpectedQuantileLogLogN16.cs
@@ -46,29 +46,29 @@ namespace LandauEvalPadeApprox {
                 }
             }
 
-            //using (StreamWriter sw = new("../../../../results_disused/quantile_lower_precision152.csv")) {
-            //    sw.WriteLine("u:=-log2(p),x:=quantile(p)");
+            using (StreamWriter sw = new("../../../../results_disused/quantile_lower_precision152.csv")) {
+                sw.WriteLine("u:=-log2(p),x:=quantile(p)");
 
-            //    MultiPrecision<N20> x = median;
+                MultiPrecision<N20> x = median;
 
-            //    for (MultiPrecision<N20> u0 = 1; u0 < 1048576; u0 *= 2) {
-            //        for (MultiPrecision<N20> u = u0; u < u0 * 2 || u == 1048576; u += u0 / (u0 < 4 ? 32768 : 16384)) {
-            //            MultiPrecision<N20> p = MultiPrecision<N20>.Pow2(-u);
+                for (MultiPrecision<N20> u0 = 1; u0 < 1048576; u0 *= 2) {
+                    for (MultiPrecision<N20> u = u0; u < u0 * 2 || u == 1048576; u += u0 / (u0 < 4 ? 32768 : 16384)) {
+                        MultiPrecision<N20> p = MultiPrecision<N20>.Pow2(-u);
 
-            //            x = NewtonRaphsonFinder<N20>.RootFind(
-            //                x => (
-            //                    CDFPadeN16.Value(x.Convert<Pow2.N16>(), complementary: false).Convert<N20>() - p, 
-            //                    PDFPadeN16.Value(x.Convert<Pow2.N16>()).Convert<N20>()
-            //                ),
-            //                x0: x, overshoot_decay: true, iters: 256, accurate_bits: MultiPrecision<Pow2.N16>.Bits
-            //            );
+                        x = NewtonRaphsonFinder<N20>.RootFind(
+                            x => (
+                                CDFPadeN16.Value(x.Convert<Pow2.N16>(), complementary: false).Convert<N20>() - p,
+                                PDFPadeN16.Value(x.Convert<Pow2.N16>()).Convert<N20>()
+                            ),
+                            x0: x, overshoot_decay: true, iters: 256, accurate_bits: MultiPrecision<Pow2.N16>.Bits
+                        );
 
-            //            Console.WriteLine($"{u}\n{x:e20}");
+                        Console.WriteLine($"{u}\n{x:e20}");
 
-            //            sw.WriteLine($"{u},{x}");
-            //        }
-            //    }
-            //}
+                        sw.WriteLine($"{u},{x}");
+                    }
+                }
+            }
 
             Console.WriteLine("END");
             Console.Read();
