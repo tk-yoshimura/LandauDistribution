425664e92dab4afff99f309c0514bb1e30a5c10e
tk-yoshimura <yoshimura8192@gmail.com>
2024-03-03 02:35:42 +0900

    cleanup

---
+++ AsymptoticExpansion/LandauEvalPDFAsymptotic/EvalPDFNegativeCoefs.cs
@@ -0,0 +1,16 @@
+﻿using MultiPrecision;
+
+namespace LandauEvalPDFAsymptotic {
+    class EvalPDFNegativeCoefs {
+        static void Main() {
+            using (StreamWriter sw = new("../../../../../results_disused/pdf_minuscoef_precision155.txt")) {
+                for (int i = 0; i < 64; i++) {
+                    sw.WriteLine($"{PDFNegativeSide<Pow2.N32>.Coefs[i]:e155}");
+                }
+            }
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+    }
+}
